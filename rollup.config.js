330683753043b46b177fe6dcb5c1be8b9fdded74
Kristján Oddsson <koddsson@gmail.com>
2023-03-03 10:33:30 +0100

    Fix imports by reexporting named exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: "./node_modules/msw/lib/index.js",
+  input: "./index.js",
   output: {
-    file: "./index.js",
+    dir: "./dist",
     format: "es",
   },
   plugins: [
